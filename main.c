d6e4d6f363a05aa64992ba4423fa7d5991ec8da7
Chevillat <jerome@smoothplanet.com>
2019-02-20 10:36:50 +0100

    Integrate a function to compute the factorial of an integer

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int factorial(int f) {
+    int total=f;
+    for (int i=f-1; i>0; i--) {
+        total=total * i;
+    }
+    return total;
+}
+
 int main()
 {
     int fact=4;
-    int total=fact;
-    for (int i=fact-1; i>0; i--) {
-        total=total * i;
-    }
-    printf("%d! = %d \n", fact, total);
+    printf("%d! = %d \n", fact, factorial(fact));
     return 0;
 }
